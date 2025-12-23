734da15515ca10bf194b0f79fcd769b9234f7d86
AYOUB <ayoubbba838@gamil.com>
2025-12-24 00:03:23 +0100

    final

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "/ecommerce-pharmacy/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
